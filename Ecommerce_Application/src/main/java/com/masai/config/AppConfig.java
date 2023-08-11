7acd3e75e0b04a2615d853f600d353fe131f22dc
Soumit-Das <soumitdas500@gmail.com>
2023-08-11 13:57:40 +0530

    appconfig class debugged

--- Ecommerce_Application/src/main/java/com/masai/config/AppConfig.java
+++ Ecommerce_Application/src/main/java/com/masai/config/AppConfig.java
@@ -47,22 +47,49 @@ public class AppConfig {
 					});
 
 				}).authorizeHttpRequests(auth -> {
-//					auth
-//					.requestMatchers(HttpMethod.POST,"/registerCustomer").permitAll()
-//					.requestMatchers(HttpMethod.GET, "/customers","/hello","/Products/getProductById/**","/Products/getAllProducts").hasRole("ADMIN")
-//					.requestMatchers(HttpMethod.POST, "/Products/addProduct").hasRole("ADMIN")
-//					.requestMatchers(HttpMethod.GET, "/customers/**").hasAnyRole("ADMIN","USER")
-//					.anyRequest().authenticated();
-					
-					
-					auth.requestMatchers(HttpMethod.POST, "/registerCustomer").permitAll()
-							.requestMatchers(HttpMethod.GET, "/customers", "/hello", "/Products/getProductById/**",
-									"/Products/getAllProducts").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.POST, "/Products/addProduct").hasRole("ADMIN")
-							.requestMatchers(HttpMethod.POST, "Cart/addProductToCart/**/**/**").hasAnyRole("ADMIN", "USER")
-//							.requestMatchers(HttpMethod.GET, "/customers/**","Cart/updateProductQuantity/**/**/**","/Products/getAllProducts").hasAnyRole("ADMIN", "USER")
-							.requestMatchers(HttpMethod.PUT, "Cart/deleteProductFromCart/{cid}/**").hasAnyRole("ADMIN", "USER").anyRequest()
-							.authenticated();
+				
+					auth
+					.requestMatchers(HttpMethod.POST, "/registerCustomer").permitAll()
+		            .requestMatchers(HttpMethod.GET, "/hello").permitAll()
+		            .requestMatchers(HttpMethod.POST, "/Customer/registerCustomer").permitAll()
+		            .requestMatchers(HttpMethod.GET, "/Products/getProductById/**").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.GET, "/Products/getAllProducts").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.POST, "/Products/addProduct").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.POST, "/Address/addAdress").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.POST, "/Category/addCategory").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.GET, "/Category/getAllProductsByCategoryId/**").hasRole("ADMIN") // ❌ controller method incorrect
+		            .requestMatchers(HttpMethod.PUT, "/Category/updateCategoryById/**").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.DELETE, "/Category/deleteCategoryById/**").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.GET, "/Category/getAllCategory").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.POST, "/Feedback/addFeedback").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.GET, "/Feedback/getAllProductsByCategoryId/**").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.PUT, "/Feedback/updateCategoryById/**").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.DELETE, "/Feedback/deleteCategoryById/**").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.GET, "/Feedback/getAllCategory").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.POST, "/Orders/createOrder").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.GET, "/Orders/getOrderById/**").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.PUT, "/Orders/updateOrders/**").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.DELETE, "/Orders/deleteOrders/**").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.GET, "/Orders/getAllOrders").hasRole("ADMIN") // ✅
+		            .requestMatchers(HttpMethod.GET, "/Address/getAddressByCustomerId/**").hasAnyRole("ADMIN", "USER") // ❌ controller method incorrect
+		            .requestMatchers(HttpMethod.PUT, "/Address/updateAddressByCustomerId/**").hasAnyRole("ADMIN", "USER") // ❌ controller method incorrect
+		            .requestMatchers(HttpMethod.DELETE, "/Address/deleteAddressByCustomerId/**").hasAnyRole("ADMIN", "USER") // ❌ controller method incorrect
+		            .requestMatchers(HttpMethod.POST, "/Cart/addProductToCart/**/**/**").hasAnyRole("ADMIN", "USER") // ❌ controller method incorrect
+		            .requestMatchers(HttpMethod.PUT, "/Cart/deleteProductFromCart/{cid}/**").hasAnyRole("ADMIN", "USER") // ❌ controller method incorrect
+		            .requestMatchers(HttpMethod.GET, "/Cart/getCartById/**").hasAnyRole("ADMIN", "USER") // ❌ controller method incorrect
+		            .requestMatchers(HttpMethod.PUT, "/Cart/updateProductQuantity/**/**/**").hasAnyRole("ADMIN", "USER") // ✅
+		            .requestMatchers(HttpMethod.PUT, "/Cart/deleteProductFromCart/{cid}/**").hasAnyRole("ADMIN", "USER") // ❌ controller method incorrect
+		           
+		            // ❌ methods to be dubugged
+		            
+		            .requestMatchers(HttpMethod.GET, "/getCustomerById/**").hasAnyRole("ADMIN", "USER") // ✅ 
+		            .requestMatchers(HttpMethod.PUT, "/updateCustomer/**").hasAnyRole("ADMIN", "USER") // ❌ controller method incorrect
+		            .requestMatchers(HttpMethod.DELETE, "/deleteCustomer/**").hasAnyRole("ADMIN", "USER") // ✅
+		            .requestMatchers(HttpMethod.GET, "/getCustomerByname/**").hasAnyRole("ADMIN", "USER") // ✅
+		            .requestMatchers(HttpMethod.GET, "/getAllCustomers").hasAnyRole("ADMIN", "USER") // ✅
+		            .requestMatchers(HttpMethod.GET, "/getCustomerByEmail/**").hasAnyRole("ADMIN", "USER") // ✅
+		            .requestMatchers(HttpMethod.GET, "/getCustomerByPhone/**").hasAnyRole("ADMIN", "USER") // ✅
+		            .anyRequest().authenticated();
 				
 					
 				}).csrf(csrf -> csrf.disable())
